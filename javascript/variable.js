e8a077bb56d77bb3fee16f0dcb0bee2e857a8d14
ayushzAIML <theayushchakraborty@gmail.com>
2026-02-27 21:01:39 +0530

    Add javascript variable file and package config

---
+++ javascript/variable.js
@@ -0,0 +1,22 @@
+let age = 23;
+age = age+1;
+
+console.log(age);
+
+let cgpa = 8.9;
+cgpa = cgpa * 10;
+
+console.log(cgpa + "%");
+
+let side = 4;
+let area = side * side;
+console.log("Area of square is " + area);
+
+let num1 = 1;
+let num2 = 2;
+let sum = num1 + num2;
+console.log("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+let name = "John";
+console.log("Hello, " + name + "!");
+
