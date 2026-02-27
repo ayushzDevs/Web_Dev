1b8bba7be42eebee27dec0ac4ad85c61f9b74bad
ayushzAIML <theayushchakraborty@gmail.com>
2026-02-27 22:39:35 +0530

    js

---
+++ javascript/pq2.js
@@ -0,0 +1,12 @@
+let name = "Ayush Chakraborty";
+console.log(name.length-1);
+
+let first_name = name.slice(0,5);
+console.log(first_name);
+
+let sl = first_name;
+console.log(sl[0]);
+console.log(sl[4]);
+
+const str= " ";
+console.log(str.length)
