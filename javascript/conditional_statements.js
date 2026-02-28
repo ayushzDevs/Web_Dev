21d813795b658cd20995b43437113327a77d07c1
ayushzAIML <theayushchakraborty@gmail.com>
2026-02-28 20:50:36 +0530

    js

---
+++ javascript/conditional_statements.js
@@ -0,0 +1,13 @@
+a = 10;
+
+if(a<20){
+    console.log("a is less than 20");
+    let ashh =5;
+    console.log(a * ashh);
+}
+else if(a==20){
+    console.log("a is equal to 20");
+}
+else{
+    console.log("a is greater than 20");
+}
---
+++ javascript/pq3.js
@@ -0,0 +1,13 @@
+let color = "red";
+
+switch(color){
+    case "red":
+        console.log("The color is red , stop");
+        break;
+    case "yellow":
+        console.log("The color is yellow , get ready to stop");
+        break;
+    case "green":
+        console.log("The color is green , you can go")
+
+}
