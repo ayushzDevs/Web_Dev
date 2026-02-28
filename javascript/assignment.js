a6683d020ba71911e37173657429e7df66d827e1
ayushzAIML <theayushchakraborty@gmail.com>
2026-03-01 02:08:43 +0530

    js

--- javascript/assignment.js
+++ javascript/assignment.js
@@ -12,9 +12,50 @@
 
 // Q2...
 
-let name = prompt("Please enter you name");
-let age = prompt("please enter your age");
+// let name = prompt("Please enter you name");
+// let age = prompt("please enter your age");
 
-alert(`${name} is ${age} years old`);
+// alert(`${name} is ${age} years old`);
 
+
+// another Q
+
+// // let name = "ayush";
+// let name = "Baban"
+
+// if(name[0]=="A"||name[0]=="a"){
+//     console.log("It's a golden string");
+// }
+// else{
+//     console.log("It's not a golden string");
+// }
+
+
+// find the largest number in the array
+
+// let largest = 0;
+// let a = [1,2,3,67,8,0];
+// for (let i=0;i<a.length;i++){
+//     if(a[i]>largest){
+//         largest = a[i];
+//     }
+//     else{
+//         continue;
+//     }
+
+// }
+
+// console.log(largest);
+
+// Write a program to check if 2 numbers have the same last digit
+
+let num1 = 32;
+let num2 = 47853;
+
+if(num1%10==num2%10 && num1%10==2 && num2%10==2){
+    console.log("yes its 2");
+}
+else{
+    console.log("no its not 2");
+}
 
