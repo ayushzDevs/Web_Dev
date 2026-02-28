81ba534bed5f1ceb2ebb9ef26551befb5f9ca845
ayushzAIML <theayushchakraborty@gmail.com>
2026-02-28 20:55:27 +0530

    js

---
+++ javascript/pq4.js
@@ -0,0 +1,17 @@
+let asked_size = "XL";
+
+
+switch(asked_size){
+    case "S" :
+        console.log("You chose size S , price is : 50 rupees");
+        break;
+    case "M" :
+        console.log("You chose zie M , price is 100 rupees");
+        break;
+    case "L":
+        console.log("You chose size L , price is 200 rupees");
+        break;
+    case "XL":
+        console.log("You chose size XL , price is 250 rupees");
+        break;
+}
