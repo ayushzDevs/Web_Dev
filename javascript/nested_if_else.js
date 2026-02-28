aaf7adbe3c163f3087a67f8b6abf9b4a80cf197a
ayushzAIML <theayushchakraborty@gmail.com>
2026-02-28 21:02:43 +0530

    js

---
+++ javascript/nested_if_else.js
@@ -0,0 +1,19 @@
+let marks =73;
+
+if(marks>=33){
+    if(marks>=80){
+        console.log("Grade : O");
+    }
+    else if(marks<80 && marks>=70){
+        console.log("Grade : A");
+    }
+    else if(marks>=40){
+        console.log("grade : B");
+    }
+
+}
+
+else{
+    console.log("Fail")
+}
+
