d29e6384e1708bbd0dd7a9635e5a26be1dbbf601
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-05 10:23:03 +0530

    jj

---
+++ javascript/str_meths.js
@@ -0,0 +1,13 @@
+// let msg = "       ayush     ";
+// console.log(msg.trim());
+
+
+// let password = prompt("set new password");
+// let newpass = password.trim();
+// console.log(newpass);
+
+let strz = "ILoveCoding";
+console.log(strz.indexOf("Love"));
+console.log(strz.indexOf("Coding"));
+console.log(strz.indexOf("Ayush")); // -1 because it is not present in the string   
+console.log(strz.indexOf("o")); // 2 because it is the index of first occurrence of "o" in the string   
