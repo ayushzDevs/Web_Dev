03552522121bf1b261f8fb2024464118953232cc
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-05 10:44:15 +0530

    jj

--- javascript/str_meths.js
+++ javascript/str_meths.js
@@ -11,3 +11,21 @@ console.log(strz.indexOf("Love"));
 console.log(strz.indexOf("Coding"));
 console.log(strz.indexOf("Ayush")); // -1 because it is not present in the string   
 console.log(strz.indexOf("o")); // 2 because it is the index of first occurrence of "o" in the string   
+
+
+console.log("\n")
+
+let newm = "I love javascript but not more than python   ";
+console.log(newm.lastIndexOf("python")); // 36 because it is the index of last occurrence of "python" in the string   
+console.log(newm.lastIndexOf("java")); // 7 because it is the index of last occurrence of "java" in the string   
+console.log(newm.lastIndexOf("c++")); // -1 because it is not present in the string
+console.log(newm.lastIndexOf("o")); // 29 because it is the index of last occurrence of "o" in the string
+
+console.log("\n")
+
+console.log(newm.toUpperCase().trim())
+
+console.log(strz.slice(-8,-2))
+console.log(strz.slice(5))
+console.log(strz.slice(strz.length-1));
+
