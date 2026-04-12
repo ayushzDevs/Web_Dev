52a9240afd4ade3e7cfcad4e3707bf60389f7093
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-12 19:30:14 +0530

    chai

--- god_phase/app.js
+++ god_phase/app.js
@@ -1,14 +1,5 @@
-let btn = document.querySelector("button");
+let kb = document.querySelector('input');
 
-btn.addEventListener("click",function(){
-    console.log(this.innerText);
-    let randomcolor = getrandomcolor();
-    this.style.backgroundColor = randomcolor;
-})
-
-function getrandomcolor(){
-    let red = Math.floor(Math.random()*256);
-    let green = Math.floor(Math.random()*256);
-    let blue = Math.floor(Math.random()*256);
-    return `rgb(${red},${green},${blue})`;
-}
+kb.addEventListener('keyup', function(){
+    console.log("key was released");
+});
