66888f3b5cb1565845e0eb84489c54b747b90a50
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-12 19:50:42 +0530

    chai

--- god_phase/app.js
+++ god_phase/app.js
@@ -1,5 +1,20 @@
 let kb = document.querySelector('input');
 
-kb.addEventListener('keyup', function(){
-    console.log("key was released");
+kb.addEventListener('keyup', function(event){
+    console.log(event.code);
+
+    if(event.code === 'ArrowUp' || event.code === 'KeyU'){
+        console.log("character moves forward");
+
+    }
+    else if(event.code =="ArrowDown" || event.code =="KeyD"){
+        console.log("character moves backward");
+    }
+    else if(event.code =="ArrowLeft" || event.code =="KeyL"){
+        console.log("character moves left");
+    }
+    else if(event.code =="ArrowRight"|| event.code =="KeyR"){
+        console.log("character moves right");
+    }
+
 });
