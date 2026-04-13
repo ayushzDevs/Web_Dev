46f142834f3b5a12a0fb4ca35afa89478d7b0785
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-13 20:42:58 +0530

    13 april

--- god_phase/app.js
+++ god_phase/app.js
@@ -1,10 +1,32 @@
-let i = document.querySelector("#text");
-let o = document.querySelector("#output");
+let div = document.querySelector("div");
+let ul = document.querySelector("ul");
+let li = document.querySelectorAll("li");
 
+div.addEventListener("click", function(e){
+    e.stopPropagation();
 
-i.addEventListener("input",((e)=>{
-console.log(e.target.value);
-o.innerText = e.target.value;    
-}));
+    console.log("div clicked");
 
+});
 
+ul.addEventListener("click", function(e){
+
+    // e.stopPropagation();
+
+    console.log("ul clicked");
+
+});
+
+// li.forEach(function(item){
+//     item.addEventListener("click",((e)=>{
+//         console.log("li clicked");
+//         e.stopPropagation();
+//     }))
+// })
+
+for(li of li){
+    li.addEventListener("click",((e)=>{
+        console.log("li clicked");
+        e.stopPropagation();
+    }))
+};
---
+++ mini_projects/todolist/app.js
@@ -0,0 +1,22 @@
+let b = document.querySelector(".addd");
+let t = document.querySelector("#todo-input");
+let u = document.querySelector("ul");
+let li = document.querySelector("li");
+
+b.addEventListener("click",function(){
+
+    let n = document.createElement("li");
+    n.innerText = t.value;
+    u.appendChild(n)
+
+    let del_b = document.createElement("button");
+    del_b.innerText = "Delete";
+    del_b.classList.add("del");
+    n.appendChild(del_b);
+    t.value= "";
+});
+
+u.addEventListener("click",function(){
+    console.log("ul was clicked");
+});
+
