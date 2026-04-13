b0954c91a9181675cfc5b4d37a6a70129ea9177d
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-13 18:56:17 +0530

    13 april

--- god_phase/app.js
+++ god_phase/app.js
@@ -3,10 +3,20 @@ form.addEventListener("submit",((event)=>{
     event.preventDefault();
 
     
-    let username = form.querySelector(".U").value;
-    let password = form.querySelector(".P").value;
+//     let username = form.querySelector(".U").value;
+//     let password = form.querySelector(".P").value;
 
-    console.dir(form);
+//     console.dir(form);
 
-    alert(`Welcome ${username}`);
+//     alert(`Welcome ${username}`);
 }));
+
+let u = document.querySelector(".U");
+
+u.addEventListener("change",((e)=>{
+    console.log(`event changed , final value is : ${e.target.value} `);
+}));
+
+u.addEventListener("input", function(){
+    console.log(`event changed , new value added is : ${this.value} `);
+})
