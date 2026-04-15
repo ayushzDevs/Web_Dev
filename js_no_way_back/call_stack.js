66b36495bba9864ee1ba08d820981a7f32b6f972
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-15 19:20:44 +0530

    15 april

--- js_no_way_back/call_stack.js
+++ js_no_way_back/call_stack.js
@@ -2,4 +2,19 @@ function hello(){
     console.log("Hello World");
 }
 
-hello();
+function demo(){
+    hello();
+}
+
+setTimeout(function(){
+    console.log("calling demo function");
+},1000);
+
+setTimeout(function(){
+    demo();
+},5000);
+
+setTimeout(function(){
+    console.log("This is the end of the code");
+},10000);
+
