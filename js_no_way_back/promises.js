3d97d4d306c8dfbeee8942e45687a6b885cd692c
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-16 12:30:52 +0530

    16 april

--- js_no_way_back/promises.js
+++ js_no_way_back/promises.js
@@ -1,45 +1,68 @@
-function savetodb(data , success , failure){
-    let internetspeed = Math.floor(Math.random()*10) +1;
+// function savetodb(data , success , failure){
+//     let internetspeed = Math.floor(Math.random()*10) +1;
 
-    if(internetspeed>4){
-            success();
-    }
-    else{
-        failure();
-    }
+//     if(internetspeed>4){
+//             success();
+//     }
+//     else{
+//         failure();
+//     }
 
 
+// }
+
+// savetodb("ayush",
+// ()=>{
+//     console.log("success1 : data saved successfully" );
+
+//     savetodb("chakraborty",
+//         ()=>{
+//         console.log("success2 : data saved successfully" );
+
+//         savetodb("jujutsu",
+//             ()=>{
+//             console.log("success3 : data saved successfully" );
+
+//             savetodb("kaisen",
+//                 ()=>{
+//                 console.log("success4 : data saved successfully ");
+//             },
+//             ()=>{
+//                 console.log("failure1 : data could not be saved");
+//             })
+//         },
+//         ()=>{
+//             console.log("failure2 : data could not be saved");
+
+//         })
+//     },
+//     ()=>{
+//         console.log("failure3 : data could not be saved");
+//     })
+// },
+// ()=>{
+//     console.log("failure4 : data could not be saved");
+// });
+
+
+
+
+
+
+// resolve to callback hell
+
+function savetodb(data){
+
+        return new Promise((success , failure)=>{
+
+                let internetspeed = Math.floor(Math.random()*10) +1;
+                if(internetspeed>4){
+                    success("success : data was saved successfully");
+                }
+                else{
+                    failure("failure : data could not be saved");
+                }
+    })
 }
 
-savetodb("ayush",
-()=>{
-    console.log("success1 : data saved successfully" );
-
-    savetodb("chakraborty",
-        ()=>{
-        console.log("success2 : data saved successfully" );
-
-        savetodb("jujutsu",
-            ()=>{
-            console.log("success3 : data saved successfully" );
-
-            savetodb("kaisen",
-                ()=>{
-                console.log("success4 : data saved successfully ");
-            },
-            ()=>{
-                console.log("failure1 : data could not be saved");
-            })
-        },
-        ()=>{
-            console.log("failure2 : data could not be saved");
-
-        })
-    },
-    ()=>{
-        console.log("failure3 : data could not be saved");
-    })
-},
-()=>{
-    console.log("failure4 : data could not be saved");
-});
+// savetodb("ayush")
