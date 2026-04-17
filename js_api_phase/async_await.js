256cc23ff6b54ed4b3427e1dac54d1bc4a762320
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-17 20:43:49 +0530

    17 april

--- js_api_phase/async_await.js
+++ js_api_phase/async_await.js
@@ -1,20 +1,51 @@
-async function greet() {
-    throw "404 page not found";
-    // abc.abc();
-    return "Hello";
+// async function greet() {
+//     throw "404 page not found";
+//     // abc.abc();
+//     return "Hello";
+// }
+
+// greet()
+// .then((res)=>{
+//     console.log("greeted successfully")
+//     console.log("Result was: ", res);
+
+// })
+
+// .catch((err)=>{
+//     console.log("Error in greeting : ", err);
+// })
+
+// let demo = async()=>{
+//     return 5;
+// }
+
+
+
+
+
+function getnum(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            let num = Math.floor(Math.random()*10) +1;
+            console.log(num);
+            resolve();
+        }, 1000);
+    })
 }
 
-greet()
-.then((res)=>{
-    console.log("greeted successfully")
-    console.log("Result was: ", res);
 
-})
+async function demo(){
+    await getnum();
+    await getnum();
+    await getnum();
+    await getnum();
+    await getnum();
 
-.catch((err)=>{
-    console.log("Error in greeting : ", err);
-})
-
-let demo = async()=>{
-    return 5;
 }
+
+
+
+
+
+
+
--- js_no_way_back/app.js
+++ js_no_way_back/app.js
@@ -41,28 +41,59 @@ function changecolor(color,delay){
 
 
 
-changecolor("red",1000)
-.then((result)=>{
-    console.log(result);
-    return changecolor("orange",1000);
-})
 
-.then((result)=>{
-    console.log(result);
-    return changecolor("cyan",1000);
-})
+// changecolor("red",1000)
+// .then((result)=>{
+//     console.log(result);
+//     return changecolor("orange",1000);
+// })
 
-.then((result)=>{
-    console.log(result);
-    return changecolor("yellow",1000);
-})
+// .then((result)=>{
+//     console.log(result);
+//     return changecolor("cyan",1000);
+// })
 
-.then((result)=>{
-    console.log(result);
-    return changecolor("green",1000);
-})
+// .then((result)=>{
+//     console.log(result);
+//     return changecolor("yellow",1000);
+// })
 
-.then((result)=>{
-    console.log(result);
-    return changecolor("blue",1000);
-})
+// .then((result)=>{
+//     console.log(result);
+//     return changecolor("green",1000);
+// })
+
+// .then((result)=>{
+//     console.log(result);
+//     return changecolor("blue",1000);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async await efficiency coding
+async function demo(){
+    await changecolor("red",1000);
+    await changecolor("orange",1000);
+    await changecolor("cyan",1000);
+    await changecolor("yellow",1000);
+    await changecolor("green",1000);
+    await changecolor("blue",1000);
+    await changecolor("purple",1000);
+    await changecolor("pink",1000);
+    await changecolor("brown",1000);
+    await changecolor("black",1000);
+}
+
+demo();
