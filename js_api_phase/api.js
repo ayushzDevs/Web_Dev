da49169b1b5455d5e301e86e9e242c82df43b01f
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-18 11:09:16 +0530

    18 april

---
+++ js_api_phase/api.js
@@ -0,0 +1,8 @@
+let json_res = '{"fact":"A cat appetite is the barometer of its health. Any cat that does not eat or drink for more than two days should be taken to a vet.","length":132}';
+
+let valid_res = JSON.parse(json_res);
+
+console.log(valid_res.fact);
+
+let new_json = JSON.stringify(valid_res);
+console.log(new_json);
