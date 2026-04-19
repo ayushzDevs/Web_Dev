eb38bb6e02add6c25f6ff59a0e2364edb748b2fb
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-19 11:02:21 +0530

    19 april

--- js_api_phase/api.js
+++ js_api_phase/api.js
@@ -1,8 +1,20 @@
-let json_res = '{"fact":"A cat appetite is the barometer of its health. Any cat that does not eat or drink for more than two days should be taken to a vet.","length":132}';
+// let json_res = '{"fact":"A cat appetite is the barometer of its health. Any cat that does not eat or drink for more than two days should be taken to a vet.","length":132}';
 
-let valid_res = JSON.parse(json_res);
+// let valid_res = JSON.parse(json_res);
 
-console.log(valid_res.fact);
+// console.log(valid_res.fact);
 
-let new_json = JSON.stringify(valid_res);
-console.log(new_json);
+// let new_json = JSON.stringify(valid_res);
+// console.log(new_json);
+
+
+
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+    .then((response)=>{
+        console.log("Response : ",response);
+    })
+    .catch((error)=>{
+        console.log("Error : ",error);
+    })
