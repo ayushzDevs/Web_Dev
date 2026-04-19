4ce432c8ca534468871385b2addbc0045bd9e1cc
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-19 12:08:53 +0530

    19 april

--- js_api_phase/axios.js
+++ js_api_phase/axios.js
@@ -3,19 +3,36 @@ let url = "https://catfact.ninja/fact";
 async function getfacts(){
     try{
         let res = await axios.get(url);
-        console.log(res.data.fact)
+        return res.data.fact;
 
         let res2 = await axios.get(url);
-        console.log(res2.data.fact)
+        return res2.data.fact;
 
         let res3 = await axios.get(url);
-        console.log(res3.data.fact)
+        return res3.data.fact;
 
         let res4 = await axios.get(url);
-        console.log(res4.data.fact)
+        return res4.data.fact;
         
     }
     catch(e){
         console.log(`error : ${e.message}`)
     }
 }
+
+let btn = document.querySelector("#show");
+
+let facts = document.querySelectorAll("#facts li");
+
+btn.addEventListener("click", async function(){
+    console.log("button clicked")
+    try{
+        for(fact of facts){
+            let res = await getfacts();
+            fact.append(res);
+        }
+    }
+    catch(e){
+        console.log(`error : ${e.message}`)
+    }
+})
